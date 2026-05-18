8912f4c8fc2f0488ec0258454ccf476c78b5f970
Danica <danicanikolic299@gmail.com>
2026-05-18 21:35:55 +0200

    otvaranje service host

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,14 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            WeatherService service = new WeatherService();
+            ServiceHost host = new ServiceHost(service);
+
+            host.Open();
+            Console.WriteLine("Server is running. Press any key to stop.");
+            Console.ReadKey();
+            host.Close();
+            Console.WriteLine("Service is closed.");
         }
     }
 }
