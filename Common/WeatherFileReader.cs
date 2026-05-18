072db74300bff25efe843b764b3d34264731e45e
Danica <danicanikolic299@gmail.com>
2026-05-18 17:43:53 +0200

    IDisposable implemetacija

---
+++ Common/WeatherFileReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Common
+{
+    public class WeatherFileReader : IDisposable
+    {
+        private StreamReader reader;
+        private bool disposed = false;
+
+        public WeatherFileReader(string path) 
+        {
+            reader = new StreamReader(path);
+        }
+
+        public string ReadLine()
+        {
+            if (disposed)
+                throw new ObjectDisposedException(nameof(WeatherFileReader));
+            return reader.ReadLine();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    reader?.Dispose();
+                }
+                disposed = true;
+            }
+        }
+        ~WeatherFileReader()
+        {
+            Dispose(false);
+        }
+    }
+}
---
+++ Common/WeatherFileWriter .cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Common
+{
+    public class WeatherFileWriter : IDisposable
+    {
+        private StreamWriter writer;
+        private bool disposed = false;
+        public WeatherFileWriter(string path)
+        {
+            writer = new StreamWriter(path, append: true);
+        }
+
+        public void WriteLine(string line)
+        {
+            if (disposed)
+                throw new ObjectDisposedException(nameof(WeatherFileWriter));
+            writer.WriteLine(line);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    writer?.Dispose();
+                }
+                disposed = true;
+            }
+        }
+
+        ~WeatherFileWriter()
+        {
+            Dispose(false);
+        }
+    }
+}
