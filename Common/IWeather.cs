ecd43d6b88b621554a7cc8cee33beb4667b0cca1
tamara10702 <tamaraopacic02@gmail.com>
2026-05-18 02:54:08 +0200

    konfiguracija, tcp binding, ugovor, meta-zaglavlje

---
+++ Common/IWeather.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IWeather
+    {
+        [OperationContract]
+        void StartSession(string meta);
+        [OperationContract]
+        void PushSample(WeatherSample sample);
+        [OperationContract]
+        void EndSession();
+    }
+}
---
+++ Server/WeatherService.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class WeatherService : IWeather
+    {
+        public void EndSession()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PushSample(WeatherSample sample)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartSession(string meta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
