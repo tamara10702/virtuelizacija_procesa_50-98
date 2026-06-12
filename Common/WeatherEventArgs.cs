e0a9a63ec6c1396a01c22780466e899adb94cf6b
Danica <danicanikolic299@gmail.com>
2026-06-12 17:51:34 +0200

    Weather Event Args class

---
+++ Common/WeatherEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class WeatherEventArgs : EventArgs
+    {
+        public double Value { get; }
+        public string Message { get; }
+
+        public WeatherEventArgs(double value, string message)
+        {
+            Value = value;
+            Message = message;
+        }
+    }
+}
